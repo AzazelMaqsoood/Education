fc419a1920399c91294af1aeeed5be9a2e1973af
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-11-07 20:43:59 +0300

    The 4th task completed

---
+++ FourthTask/FourthTask.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+    unsigned short int a = 4, b = 2; //Cos short weights 2 bytes
+
+    cout << "А)" << "2+4=" << (a + b) << endl;
+    cout << "Б)" << "Z(" << a << ")=" << "F(" << b << ")" << endl;
+    cout << "В)" << "a=" << a << ';' << "b=" << b << ';' << endl;
+    cout << "Г)" << "Ответ:(" << a << ';' << b << ')' << endl;
+}
