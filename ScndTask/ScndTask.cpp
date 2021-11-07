71dfd2d276e00532bc5c512d7bc762fa1cbbfe44
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-11-07 19:12:14 +0300

    Complete 2nd task

---
+++ ScndTask/ScndTask.cpp
@@ -0,0 +1,12 @@
+﻿
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    cout << setw(4) << "A" <<endl << setw(5) << "AMA" << endl <<  setw(6) << "AMMMA" << endl<< "AMMMMMA" 
+      << endl << setw(6) << "MMMMM" << endl << setw(6) << "HH HH" << endl << setw(6) << "MMMMM" << endl;
+}
---
+++ ThrdTask/ThrdTask.cpp
@@ -0,0 +1,11 @@
+﻿
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	cout << setw(3) << "A";
+}
