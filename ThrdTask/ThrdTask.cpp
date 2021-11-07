01156f0a34512bc5a17630929156ac529e13411e
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-11-07 19:38:54 +0300

    The 3rd task completed

--- ThrdTask/ThrdTask.cpp
+++ ThrdTask/ThrdTask.cpp
@@ -1,5 +1,4 @@
-﻿
-
+﻿//3.Вывести на экран изображение двух домиков:
 #include <iostream>
 #include <iomanip>
 
@@ -7,5 +6,12 @@ using namespace std;
 
 int main()
 {
-	cout << setw(3) << "A";
+    cout << setw(4) << "A" << setw(8) << "A" << endl;
+    cout << setw(5) << "AMA" << setw(8) << "AMA" << endl;
+    cout << setw(6) << "AMMMA" << setw(8) << "AMMMA" << endl;
+    cout << setw(7) << "AMMMMMA" << setw(8) << "AMMMMMA" << endl;
+    cout << setw(6) << "MMMMM" << setw(8) << "MMMMM" << endl;
+    cout << setw(6) << "HH HH" << setw(8) << "HH HH" << endl;
+    cout << setw(6) << "MMMMM" << setw(8) << "MMMMM" << endl;
+
 }
