0ccf2019584d54a828e1a2834d879c1cb6868627
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-11-08 21:23:38 +0300

    The 7th task completed

---
+++ SvnthTask/SvnthTask.cpp
@@ -0,0 +1,18 @@
+﻿//5)Ввести с клавиатуры три целых числа, найти их сумму, 
+//  произведение и среднее арифметическое. 
+
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    srand(time(NULL)); //init func srand by value of func time
+    unsigned short int x = rand() % 1000; //gen random number
+    cout << x << endl;
+    cout << "x содержит: " << x / 100 << "сотен(ю)" << endl;
+    cout << "x содержит: " << x / 10 << "десятков(а)" << endl;
+    cout << "x содержит: " << x % 10 << "единиц(ы)" << endl;
+}
