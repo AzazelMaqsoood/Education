d5e7fd5e04ee35880de40a413df55b317cada1af
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-11-09 21:34:16 +0300

    The 16th task version 1.0

---
+++ SixthTask/SixthTask.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    float a, b;
+    srand(time(NULL));
+    cin >> a;
+    cin >> b;
+    cout << (float)rand() / RAND_MAX * (b - a) << endl;
+    cout << (float)rand() / RAND_MAX * (b - a) << endl;
+    cout << (float)rand() / RAND_MAX * (b - a) << endl;
+
+}
