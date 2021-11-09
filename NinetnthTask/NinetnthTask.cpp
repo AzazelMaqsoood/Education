bd42341443466a47a8216e073e5c946ae529ada1
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-11-09 19:06:38 +0300

    The 19th task completed

---
+++ NinetnthTask/NinetnthTask.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double firstMass, secondMass;
+	short int days;
+
+	cout << "Введите количество фруктов: ";
+	cin >> firstMass;
+	cout << "Введите количество дней: ";
+	cin >> days;
+	secondMass = firstMass - (days * 0.99); //By condition to loss 1% per day
+	cout << "Масса после хранения: " << secondMass;
+}
