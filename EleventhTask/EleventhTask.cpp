ce1da292c0ee9e0389420f7fd472e8133175b3f6
Максим Шпирко <max-essential@bk.ru>
2021-11-11 19:29:26 +0300

    The 11th task completed

---
+++ EleventhTask/EleventhTask.cpp
@@ -0,0 +1,20 @@
+﻿// EleventhTask.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+
+	short int x, y;
+
+	cin >> x;
+	cin >> y;
+	cout << pow(x, y);
+
+}
+
