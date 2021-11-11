a08ef63247023c4c24909f31c7954828f10f2d3e
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-11-11 22:29:54 +0300

    The 15th task completed

---
+++ FiftnthTask/FiftnthTask.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    int a, b, c;
+    cout << "Введите число А: ";
+    cin >> a;
+    cout << "Введите число B, которое меньше чем А: ";
+    cin >> b;
+    if (a < b) {
+        srand(a * b);
+        for (int i = 1; i <= 3; i++) {
+            c = rand() % (b - a) + a;
+            cout << "Число " << i << " в пределах интервала [" << a << "," << b << "]: " << c << endl;
+        }
+    }
+    else {
+        cout << "Ошибка: не выполнено условие";
+    }
+
+    return 0;
+}
