b477c11381c909d7f9a992dd3112d4ce64d16dff
Максим Шпирко <max-essential@bk.ru>
2021-11-11 19:26:27 +0300

    The 8th task comleted

--- EighthTask/EighthTask.cpp
+++ EighthTask/EighthTask.cpp
@@ -1,8 +1,24 @@
 ﻿
-
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "ru");
+
+    short int x, y, z;
+
+    cout << "Введите x:";
+    cin >> x;
+    cout << "Введите y:";
+    cin >> y;
+    
+    z = y;
+    y = x;
+    x = z;
+
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
+
 }
