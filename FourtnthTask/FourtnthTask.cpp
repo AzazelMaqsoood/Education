64c9c0cd3111bad271227ddf6c821758237a506b
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-11-11 22:26:58 +0300

    The 14th task completed

--- FourtnthTask/FourtnthTask.cpp
+++ FourtnthTask/FourtnthTask.cpp
@@ -1,20 +1,22 @@
-﻿
-#include <iostream>
-#include <ctime>
+﻿#include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-	int a, b;
+    setlocale(LC_ALL, "Russian");
+    srand(time(0));
+    int students_count, first_student, second_student;
+    cout << "Укажите количество студентов в группе: ";
+    cin >> students_count;
+    first_student = rand() % students_count + 2;
+    second_student = rand() % students_count + 2;
+    while (second_student == first_student) {
+        second_student = rand() % students_count + 2;
+    }
+    cout << "Дежурные по группе: " << first_student << " и " << second_student << endl;
 
-	setlocale(LC_ALL, "Ru");
-
-	cin >> a;
-	cin >> b;
-	srand(time(NULL));
-
-	cout << (rand() % (a - b) + b) << endl;
-	cout << rand() % (a - b) + b << endl;
-	cout << rand() % (a - b) + b << endl;
+    return 0;
 }
+
