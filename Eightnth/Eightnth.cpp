d2b7ca32b9684b245df787bd6903a2c777a51a30
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-11-11 20:54:30 +0300

    The 18th task completed

---
+++ Eightnth/Eightnth.cpp
@@ -0,0 +1,24 @@
+﻿Code, Compile, Runand Debug C++ program online.
+Write your code in this editorand press "Run" button to compile and execute it.
+
+* ******************************************************************************/
+
+#include <iostream> 
+#include <iomanip> 
+#include <cmath> 
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float S, l;
+    int R;
+    cout << "Введите радиус круга: ";
+    cin >> R;
+    S = M_PI * pow(R, 2);
+    l = 2 * M_PI * R;
+    cout << "Длина окружности: " << l << endl << "Площадь круга: " << S;
+
+    return 0;
+}
