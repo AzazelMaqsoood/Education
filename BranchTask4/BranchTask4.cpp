6ff40cff946c50a9e13074977fbfaa5a260be36d
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-11-18 21:44:05 +0300

    Task completed

---
+++ BranchTask4/BranchTask4.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    
+    int a;
+    cout << "Введите четырехзначное число: ";
+    cin >> a;
+    int temp = a;
+    int b = 0;
+
+    if (a/1000<=0.1 || a/10000>=1)
+    {
+        cout << "Введено не четырехзначное число";
+    }
+    while (temp != 0)
+    {
+        b = b * 10 + temp % 10;
+        temp /= 10;
+    }
+    string rez = (a == b) ? "Полиндром": "Не полиндром";
+
+    cout << rez;
+}
+
