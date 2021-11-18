639790d27b0031abd5056def2c4d63d74c48b057
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-11-18 22:31:49 +0300

    Task complated

---
+++ BranchTask5/BranchTask5.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    int a, b, c;
+    cout << "Введите первое число: ";
+    cin >> a;
+    cout << "Введите второе число: ";
+    cin >> b;
+    cout << "Введите третье число: ";
+    cin >> c;
+    bool isNegative;
+    isNegative = (a != abs(a) || b != abs(b) || c != abs(c)); //Comare nums with their modules
+    cout << "Число отрицательное ? - " << boolalpha << isNegative;
+}
+
