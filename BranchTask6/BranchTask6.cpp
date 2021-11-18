894712998793a0408db3084d752843411d4402dd
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-11-18 23:11:24 +0300

    Task completed

---
+++ BranchTask6/BranchTask6.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    int a, b, c;
+    bool isDoubleDigit;
+    cout << "Введите первое число: ";
+    cin >> a;
+    cout << "Введите второе число: ";
+    cin >> b;
+    cout << "Введите третье число: ";
+    cin >> c;
+    isDoubleDigit = (a / 10.0 >= 1 && b / 10.0 >= 1 && c / 10.0 >= 1);
+    cout << "Все числа двузначные? - " << boolalpha << isDoubleDigit;
+}
+
