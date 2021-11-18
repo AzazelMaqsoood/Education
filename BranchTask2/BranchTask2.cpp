3b3ff4a19309b56ba91f45bae6c514703a87c6af
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-11-18 20:16:18 +0300

    task completed

---
+++ BranchTask2/BranchTask2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	setlocale(LC_ALL, "Ru");
+
+    short int a;
+	cout << "Введите число: ";
+    cin >> a;
+	if (a > 0)
+		cout << "число положительное";
+	else if (a < 0)
+		cout << "число отрицательное";
+	else if (a = 0)
+		cout << '0';
+	else cout << "Введено не число";
+}
