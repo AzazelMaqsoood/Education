ed648ff675d5edd762984a03a36c6e3b49ef88c8
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-11-18 20:33:57 +0300

    task completed

---
+++ BranchTask3/BranchTask3.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    short int a;
+    cout << "Введите число: ";
+    cin >> a;
+    if (a % 2 != 0) //Checking the remainder of the devision
+    {
+        cout << "Нечетное";
+    }
+    else cout << "Четное";
+}
