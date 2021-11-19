fe0fefd0fe5ce278cf24740b02a715bb0ad793c6
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-11-19 19:47:34 +0300

    The task completed

---
+++ BranchTask10/BranchTask10.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    short int month;
+
+    cout << "Введите номер месяца";
+    cin >> month;
+    if (month == 12 || month == 1 || month == 2)
+    {
+        cout << "Бррр... Зима";
+    }
+    else if (month == 3 || month == 4 || month == 5)
+    {
+        cout << "Весна, весна! Как воздух чист! Как ясен небосклон!";
+    }
+    else if (month == 6 || month == 7 || month == 8)
+    {
+        cout << "Лето пахнет земляникой, Тёплым дождиком, клубникой";
+    }
+    else if (month == 9 || month == 10 || month == 11)
+    {
+        cout << "Мдее...Осень";
+    }
+    else {
+        cout << "Ошибочка. В году всего 12 месяцев";
+    }
+}
+
