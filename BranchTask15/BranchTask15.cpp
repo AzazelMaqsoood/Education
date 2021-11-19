938ea0ff071ee3421e64727354c8270aa6974416
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-11-19 21:33:38 +0300

    Task completed

---
+++ BranchTask15/BranchTask15.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    double pointX, pointY;
+    short int R = 2;
+    bool isIncluded;
+    
+    cout << "Введите координаты точки: ";
+    cin >> pointX >> pointY;
+    isIncluded = (pointX <= 2 && pointY <= pointX && pow(pointX, 2)+ pow(pointY, 2) >= pow(R, 2));
+    cout << "Точка входит в область - " << boolalpha << isIncluded;
+}
+
