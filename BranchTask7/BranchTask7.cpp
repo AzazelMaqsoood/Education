5ab18990ce19b7602819b66200799bbcc42b2fcb
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-11-19 19:03:40 +0300

    Task completed

---
+++ BranchTask7/BranchTask7.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    short int a, b, c;
+    bool isLong;
+    cout << "Введите длину первой веревки: ";
+    cin >> a;
+    cout << "Введите длину второй веревки: ";
+    cin >> b;
+    cout << "Введите длину третьей веревки: ";
+    cin >> c;
+    isLong = ((a + b > 30) || (a + c > 30) || (b + c > 30));
+    cout << "Длина полученной веревки больше 30? - " << boolalpha << isLong;
+
+}
+
