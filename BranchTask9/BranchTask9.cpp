0ab326737692e1780a64c06a2d0010e5cb6dce27
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-11-19 19:35:38 +0300

    Task completed

---
+++ BranchTask9/BranchTask9.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    short int x, y;
+    bool isOpposite;
+    bool isZero;
+
+    cout << "Введите числа x и y: ";
+    cin >> x >> y;
+    isOpposite = (x * y != abs(x * y));
+    cout << boolalpha << isOpposite << endl;
+    isZero = (x == 0 || y == 0);
+    cout << boolalpha << isZero << endl;
+    isZero = (x*y == 0 && x+y != 0);
+    cout << boolalpha << isZero << endl;
+}
+
