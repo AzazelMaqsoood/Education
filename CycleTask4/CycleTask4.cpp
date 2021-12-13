61476d25f8c890e0b68233b9c256f6901de6f06e
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-12-13 21:46:32 +0300

    The task completed

---
+++ CycleTask4/CycleTask4.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+    short int a, b, qrt = 0;
+
+	cin >> a;
+	cin >> b;
+
+	do
+	{
+		qrt = pow(a, 2);
+		cout << a << " ^ 2 = " << qrt << endl;
+		a++;
+	} while (a <= b);
+}
+
