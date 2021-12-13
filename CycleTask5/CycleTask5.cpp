84a698c161e7a43f1b69f68628a411c0fb0e3e60
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-12-13 21:51:26 +0300

    The task completed

---
+++ CycleTask5/CycleTask5.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+
+	short int a, b, sum = 0;
+
+	cin >> a;
+	cin >> b;
+
+	do
+	{
+		sum += pow(a, 2);
+		a++;
+	} while (a <= b);
+
+	cout << sum;
+}
+
