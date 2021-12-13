fb49a9c8f636ed279e02c391709ac67c0af6d2f7
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-12-13 21:02:36 +0300

    The task completed

---
+++ CycleTask2/CycleTask2.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+    short int number, result = 0;
+
+    cin >> number;
+
+    for (short i = 0; i <= number; i++)
+    {
+        result += i;
+    }
+    
+    cout << result;
+}
+
