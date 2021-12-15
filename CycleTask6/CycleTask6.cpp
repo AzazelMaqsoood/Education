51ba923484a318ee34da6f12282cdd9e075335c0
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-12-15 21:06:46 +0300

    The task completed

---
+++ CycleTask6/CycleTask6.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+    short int a, b, N, rnd;
+
+    cin >> a;
+    cin >> b;
+    cin >> N;
+
+    srand(time(0));
+
+    for (int i = 1; i <= N; i++)
+    {
+        rnd = a + rand() % (b-a);
+        cout << rnd << ' ';
+    }
+
+}
+
