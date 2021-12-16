497664e86a9da238322f37b2368b4b11892ec54f
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-12-16 19:02:26 +0300

    The task completed

---
+++ CycleTask8/CycleTask8.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+    int x, sum = 0, pro = 1;
+    while (true)
+    {
+        cin >> x;
+        if (x == 0) break;
+        sum += x;
+        pro *= x;
+    }
+    cout << sum << " " << pro;
+    return 0;
+}
+
