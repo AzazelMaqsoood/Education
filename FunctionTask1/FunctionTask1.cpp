32db80a77ed9a756f192572cd76495e10261dd81
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-12-16 19:55:03 +0300

    The task completed

---
+++ FunctionTask1/FunctionTask1.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+void ReverseNumber(int number)
+{
+    number = (number % 10) * 10 + number / 10;
+    cout << number;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    int number;
+
+    cout << "Введите двузначное число: ";
+    cin >> number;
+
+    ReverseNumber(number);
+}
+
