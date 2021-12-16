f497fbb8aca8ccea618e813bef802d8382dba8ab
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-12-16 20:59:25 +0300

    The task completed

---
+++ FucntionTask6/FucntionTask6.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+long Factorial(int N)
+{
+    long f = 1;
+    for (int i = 1; i <= N; i++)
+    {
+        f *= i;
+    }
+    return f;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    long N;
+    cin >> N;
+    cout << "Факториал введенного числа: " << Factorial(N);
+}
+
