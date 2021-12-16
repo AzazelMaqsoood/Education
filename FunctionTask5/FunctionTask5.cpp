71f5d1f22b377cfffeafbe1e43b3aa3901959a5d
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-12-16 20:53:23 +0300

    The task completed

---
+++ FunctionTask5/FunctionTask5.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int CountDigits(int N)
+{
+	int cnt = 0;
+	for (int i = 0; i <= N; i++)
+	{
+		N %= 10;
+		cnt = i;
+	}
+	return cnt;	
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+    short int N;
+	cin >> N;
+
+	cout << "Число имеет - " << CountDigits(N) << " знака(ов)";
+
+}
+
