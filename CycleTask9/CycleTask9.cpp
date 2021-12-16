c483fa01b5f6f19c57aab70355445ff32408b6cb
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-12-16 19:22:04 +0300

    The task completed

---
+++ CycleTask9/CycleTask9.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+    int x, min = 1, max = 0;
+    while (true)
+    {
+        cin >> x;
+        if (x == 0) break;
+        if (max < x)
+            max = x;
+        if (min > x)
+            min = x;
+    }
+    cout << max << ' ' << min;
+    return 0;
+}
+
