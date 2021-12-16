b59d9f2c0437129f171bce5cf95039aa9cc821b3
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-12-16 19:30:00 +0300

    The task completed

---
+++ CycleTask10/CycleTask10.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+	long N, f = 1;
+	cin >> N;
+	for (int i = 1; i <= N; i++)
+	{
+		f *= i;
+	}
+	cout << f;
+}
+
