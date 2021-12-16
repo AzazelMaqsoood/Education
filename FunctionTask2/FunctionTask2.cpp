5ddaa064541361370248e9d2c65b8de0e6bbb154
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-12-16 20:02:37 +0300

    The task completed

---
+++ FunctionTask2/FunctionTask2.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+void ShowDeviders(short int N) {
+    for (short int i = 1; i <= N; i++)
+    {
+        if (N % i == 0)
+        {
+            cout << i << ' ';
+        }
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+    short int N;
+
+    cin >> N;
+
+    ShowDeviders(N);
+    
+}
+
