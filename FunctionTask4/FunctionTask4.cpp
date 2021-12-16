9e2926f159fb10463444615a296fd7c35ba5c9d9
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-12-16 20:39:43 +0300

    The task completed

---
+++ FunctionTask4/FunctionTask4.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+char CheckMax(int a ,int b ,int c) 
+{
+    if (a > b && a > c)
+    {
+        return 'a';
+    }
+    else if (b > a && b > c)
+    {
+        return 'b';
+    }
+    else
+        return 'c';
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+    short int a, b, c;
+    cout << "Введите число а: ";
+    cin >> a;
+    cout << "Введите число b: ";
+    cin >> b;
+    cout << "Введите число c: ";
+    cin >> c;
+
+    cout << "Наибольшее число: " << CheckMax(a, b, c);
+}
+
