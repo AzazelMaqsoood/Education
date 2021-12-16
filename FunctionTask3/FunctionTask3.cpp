cf90d4bbc44e59506c588161a15935dd06c0d524
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-12-16 20:28:17 +0300

    The task completed

---
+++ FunctionTask3/FunctionTask3.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+string CheckSimple(short int N) 
+{
+	for (short int i = 2; i < N; i++)
+	{
+		N %= i;
+		if (N % i == 0)
+		{
+			return "Составное";
+		}
+		else {
+			return "Простое";
+		}
+	}
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+    short int N;
+
+	cin >> N;
+	cout << CheckSimple(N);
+}
+
