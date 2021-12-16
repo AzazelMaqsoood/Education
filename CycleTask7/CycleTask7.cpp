e000cacfcd93539f0b17d3d353474436fe3fd4fd
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2021-12-16 18:33:26 +0300

    The task completed

---
+++ CycleTask7/CycleTask7.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+    short int number, rez = 0;
+
+    cin >> number;
+
+    while (number > 0)
+    {
+        rez += number % 10;
+        number /= 10;
+    }
+    cout << rez;
+}
+
