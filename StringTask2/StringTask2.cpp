3db083249bc68c6d778a6b55efd943ea9920a66b
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2022-03-08 12:22:08 +0300

    The string task 2  completed

---
+++ StringTask2/StringTask2.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+    string str;
+    short num = 0;
+
+    cout << "Введите строку: ";
+    cin >> str;
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == 'c') {
+            str[i] = 'v';
+            num += 1;
+        }
+        else if (str[i] == 'C') {
+            str[i] = 'V';
+            num += 1;
+        }
+
+    }
+    cout << "Новая строка: " << str << endl << "Количество замен: " << num;
+}
+
