914a25ea489f7edace9fc4d279da7909e9f80678
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2022-03-09 18:26:05 +0300

    String task 4 completed

---
+++ StringTask4/StringTask4.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+    string str;
+    short charIndex;
+
+    cin >> str;
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == ' ')
+        {
+            charIndex = str[i];
+        }
+        str.resize(str.length() - (str.length() - charIndex));
+        break;
+    }
+
+
+
+    cout << str;
+
+}
+
