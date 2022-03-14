fe19046f177f512481d5b2d8f3b78d5f4797ad28
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2022-03-14 19:51:57 +0300

    String task 9 completed

---
+++ StringTask9/StringTask9.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+    string str;
+    string temp;
+
+    getline(cin, str);
+   
+    str.erase(remove(str.begin(), str.end(), ' '), str.end());
+    
+    temp = str;
+    reverse(str.begin(), str.end());
+    if (str.compare(temp) == 0)
+        cout << "Yes";
+    else
+        cout << "No";
+}
