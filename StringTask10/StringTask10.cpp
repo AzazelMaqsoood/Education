a2408bbaff8431ad07d85510a054c796ee0a5f7f
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2022-03-15 19:10:10 +0300

    String task 10 completed

---
+++ StringTask10/StringTask10.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "RUS");
+    string s{ "C:\\WINDOWS\\WORK\\my.txt" };
+    int indx = 0;
+    while (true) {
+        int indx1 = s.find('\\', indx);
+        if (indx1 == string::npos) {
+            break;
+        }
+        else {
+            cout << s.substr(indx, indx1) << endl;
+            s.erase(indx, indx1 + 1);
+        }
+    }
+    cout << s << endl;
+    return 0;
+}
