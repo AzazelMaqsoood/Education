d17ec0a1da07ede34d9061c2381f78e4a78c6afa
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2022-03-17 19:02:46 +0300

    String task 14

---
+++ StringTask14/StringTask14.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <clocale>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    string str;
+    string substr;
+
+    getline(cin, str);
+    getline(cin, substr);
+
+
+    short pos = 0, cnt = 0;
+
+    while ((pos = str.find(substr, pos)) != string::npos)
+    {
+        pos += substr.size();
+        cnt++;
+    }
+    cout << str << endl << "Количество вхождений: " << cnt;
+}
