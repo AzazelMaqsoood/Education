ce1755eac6e756bb83c193df0ac8556cf01e4b10
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2022-03-17 18:59:27 +0300

    String task 13 completed

---
+++ StringTask13/StringTask13.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <clocale>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    string str;
+    string toReplace;
+    string replaceWith;
+
+    getline(cin, str);
+    getline(cin, toReplace);
+    getline(cin, replaceWith);
+
+    short pos = 0, cnt = 0;
+
+    while ((pos = str.find(toReplace, pos)) != string::npos)
+    {
+        str.replace(pos, toReplace.size(), replaceWith);
+        pos += replaceWith.size();
+        cnt++;
+    }
+    cout << str << endl << "Количество замен: " << cnt;
+}
