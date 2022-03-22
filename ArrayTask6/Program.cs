7bf1631c4baacb45b9bba43ae7a2252b627fc1de
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2022-03-22 20:16:00 +0300

    Array task 6 completed

---
+++ ArrayTask6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ArrayTask6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n + 1];
+
+            for (int i = n; i >= 1; i--)
+            {
+                array[i] = (int)Math.Pow(2, i);
+                Console.Write("{0} ", array[i]);
+            }
+        }
+    }
+}
