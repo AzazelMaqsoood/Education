4e0015f9bf8cb09315c3d0619b74370877c518b2
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2022-03-22 20:40:09 +0300

    Array task 8 completed

---
+++ ArrayTask8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ArrayTask7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int even = 0;
+            int odd = 0;
+            int n = Convert.ToInt32(Console.ReadLine());
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Random rnd = new Random();
+
+            int[] array = new int[n + 1];
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = rnd.Next(a, b);
+                Console.WriteLine("{0} ", array[i]);
+                if (array[i] % 2 != 1)
+                    even++;
+                else
+                    odd++;
+            }
+            Console.WriteLine("Четных: {0}", even);
+            Console.WriteLine("Нечетных: {0}", odd);
+        }
+    }
+}
