e4fbc9e2c8e8c199d5f22f8a864a39fee75656e0
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2022-03-22 19:47:41 +0300

    Array task 1 completed

--- ArrayTask1/Program.cs
+++ ArrayTask1/Program.cs
@@ -6,15 +6,13 @@ namespace ArrayTask1
     {
         static void Main(string[] args)
         {
-            int n, x;
+            int n;
             n = Convert.ToInt32(Console.ReadLine());
-            x = Convert.ToInt32(Console.ReadLine());
             int[] array = new int[n+1];
 
             for (int i = 1; i <= n; i++)
             {
-                array[i] = x;
-                x++;
+                array[i] = i;
                 Console.Write("{0} ", array[i]);
             }
 
