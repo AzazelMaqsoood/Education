4c6bfc0cb0ba4d3f2fda7224d4b6bd135d0dfc66
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2022-03-22 20:31:21 +0300

    Array task 7 completed

---
+++ ArrayTask7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ArrayTask7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cnt = 0;
+            int n = Convert.ToInt32(Console.ReadLine());
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Random rnd = new Random();
+
+            int[] array = new int[n + 1];
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = rnd.Next(a,b);
+                Console.WriteLine("{0} ", array[i]);
+                if (array[i] % 2 != 1)
+                    cnt++;
+
+            }
+            Console.WriteLine(cnt);
+        }
+    }
+}
