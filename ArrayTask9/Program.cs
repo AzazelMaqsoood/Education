94e66b83917679dd215ebc0768516b20d7f27866
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2022-03-23 19:09:29 +0300

    Array task 9 completed

---
+++ ArrayTask9/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ArrayTask9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            int a = 0;
+            int b = 0;
+            int result = int.MinValue;
+
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[n + 1];
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = rnd.Next(a,b);
+                Console.WriteLine("{0} ", array[i]);
+                if (array[i] > result)
+                {
+                    result = array[i];
+                }
+                else if (result < 0)
+                {
+                    result = -1;
+                }
+                
+            }
+            Console.WriteLine(":{0}", result);
+        }
+    }
+}
