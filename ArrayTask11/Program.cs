3b7f34ac41f2abe381401f3157658d57d2d1ba89
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2022-03-23 19:25:44 +0300

    Array task 11 completed

---
+++ ArrayTask11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ArrayTask11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            int x = Convert.ToInt32(Console.ReadLine());
+            Random rnd = new Random();
+            int equalCnt = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                array[i] = rnd.Next(1, 5);
+                Console.WriteLine("{0} ",array[i]);
+                if (array[i] == x)
+                    equalCnt++;
+            }
+            Console.WriteLine(equalCnt);
+        }
+    }
+}
