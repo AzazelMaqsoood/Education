e0819cad055d4d2d978670b3483ba2b70130646f
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2022-03-24 19:59:53 +0300

    Array task 13 completed

--- ArrayTask13/Program.cs
+++ ArrayTask13/Program.cs
@@ -6,10 +6,11 @@ namespace ArrayTask11
     {
         static void Main(string[] args)
         {
+            int max = 0;
             int cnt = 0;
             int n = Convert.ToInt32(Console.ReadLine());
             int[] array = new int[n];
-            int x = Convert.ToInt32(Console.ReadLine());
+            //int x = Convert.ToInt32(Console.ReadLine());
             Random rnd = new Random();
 
             for (int i = 0; i < n; i++)
@@ -17,15 +18,20 @@ namespace ArrayTask11
                 array[i] = rnd.Next(1, 5);
                 Console.Write("{0} ", array[i]);
                 Console.WriteLine();
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
             }
             for (int i = 0; i < n; i++)
             {
-                if (array[i] == x)
+                if (array[i] == max)
                 {
-                    cnt++;
-                    Console.Write("- {0} ", cnt);
+                    cnt++; 
                 }
             }
+
+            Console.Write("Max: {0}, Count: {1}", max, cnt);
 
         }
     }
