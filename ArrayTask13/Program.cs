cfb1efc2def70f32a1e8db9d9370635fc062b24b
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2022-03-24 19:50:09 +0300

    Array task 13 completed

--- ArrayTask13/Program.cs
+++ ArrayTask13/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 
-namespace ArrayTask13
+namespace ArrayTask11
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int cnt = 0;
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+            int x = Convert.ToInt32(Console.ReadLine());
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = rnd.Next(1, 5);
+                Console.Write("{0} ", array[i]);
+                Console.WriteLine();
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (array[i] == x)
+                {
+                    cnt++;
+                    Console.Write("- {0} ", cnt);
+                }
+            }
+
         }
     }
 }
