2a67f29acdd70c334b63798576a0a42ba099063d
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2022-03-24 20:13:51 +0300

    Array task 14 completed

---
+++ ArrayTask14/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ArrayTask14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+            Random rnd = new Random();
+            int max = 0;
+            int min = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = rnd.Next(-10, 10);
+                if (array[i] > max && array[i]%2 == 0 && array[i] > 0)
+                {
+                    max = array[i];
+                }
+                else if (array[i] < min && array[i] % 2 == 0 && array[i] > 0)
+                {
+                    min = array[i];
+                }
+                Console.Write("{0}", array[i]);
+                Console.WriteLine();
+            }
+            Console.WriteLine("max: {0} , min: {1}", max, min);
+        }
+    }
+}
