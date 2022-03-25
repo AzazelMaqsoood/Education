9de83fd1055f95c40a85f37423785275eb5abe52
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2022-03-25 19:49:55 +0300

    Matrix task 2 completed

---
+++ MatrixTask2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MatrixTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[5,5];
+            int k = Convert.ToInt32(Console.ReadLine());
+            int cnt = 0;
+            Random rnd = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix[i, j] = rnd.Next(1, 10);
+                    if (matrix[i, j] == k)
+                        cnt++;
+                }
+            }
+            Console.WriteLine(cnt);
+        }
+    }
+}
