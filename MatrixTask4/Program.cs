c9c5ac9aa2ca6aac740c969ad56cd598053eaa24
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2022-03-30 18:45:20 +0300

    Matrix task 4 completed

--- MatrixTask4/Program.cs
+++ MatrixTask4/Program.cs
@@ -6,7 +6,41 @@ namespace MatrixTask4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] matrix = new int[5, 5];
+            int sum = 0;
+            int max = 0;
+            int j_max = 0;
+            Random rnd = new Random();
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (j % 5 == 0)
+                        Console.WriteLine();
+                    matrix[i, j] = rnd.Next(0, 10);
+                    Console.Write(matrix[i, j] + " [{0},{1}] ", i, j);
+                    sum += matrix[i, j];
+                    if (sum > max)
+                    {
+                        max = sum;
+                        j_max = j;
+                    }
+                    if (j == 4)
+                    {
+                        sum = 0;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(max);
+
+            for (int k = 0; k < matrix.GetLength(1); k++)
+            {
+                Console.Write(matrix[k, j_max] + " [{0},{1}] ", k, j_max);
+            }
         }
     }
 }
