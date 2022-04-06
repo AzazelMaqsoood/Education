3f13283684a3a3816174a32faf1e2aa37927d098
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2022-04-06 14:34:13 +0300

    Sort task 4 completed

---
+++ SortTask4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace SortTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[N];
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int c = 0; c < arr.GetLength(0); c++)
+            {
+                for (int j = 0; j < arr.GetLength(0)-1; j++)
+                {
+                    if (j < N / 2)
+                    {
+                        if (arr[j] < arr[j + 1])
+                        {
+                            int temp = arr[j];
+                            temp = arr[j + 1];
+                            arr[j + 1] = arr[j];
+                            arr[j] = temp;
+                        }
+                    }
+                    else
+                    {
+                        if (arr[j] > arr[j + 1])
+                        {
+                            int temp = arr[j];
+                            temp = arr[j + 1];
+                            arr[j + 1] = arr[j];
+                            arr[j] = temp;
+                        }
+                    }
+                }
+            }
+
+            foreach (int r in arr)
+            {
+                Console.WriteLine(r);
+            }
+        }
+    }
+}
