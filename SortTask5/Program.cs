5b17ac14fc163ab3792b50e00ac94d73fbe3e7da
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2022-04-06 15:27:55 +0300

    Sort task 5 completed

---
+++ SortTask5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace SortTask5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //заполянем массив случайными числами
+            var rnd = new Random();
+
+            int mediana = 0;
+
+            var arr = new int[10];
+
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = rnd.Next(10);
+
+            var sum = 0;
+            foreach (int r in arr)
+            {
+                Console.Write(r+ " ");
+                sum += r;
+            }
+
+            
+                //перебираем элементы, пока не достигнем 50% от суммы:
+                var accum = 0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    accum += arr[i];
+                    if (accum >= sum / 2)
+                    {
+                    mediana = i;
+                    break;
+                    }   
+                }
+            
+            Console.WriteLine();
+            Console.WriteLine(mediana);
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
