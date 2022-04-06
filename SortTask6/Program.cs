46afe40fc45eb50a6edd9698d761e86fbcda1841
AzazelMaqsoood <75746808+AzazelMaqsoood@users.noreply.github.com>
2022-04-06 15:55:49 +0300

    Sort task 6 completed

---
+++ SortTask6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SortTask6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[N];
+            Random rnd = new Random();
+            int temp = 0;
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                arr[i] = rnd.Next(1, 10);
+                Console.Write(arr[i] + " ");
+
+                for (int j = 0; j < arr.GetLength(0); j++)
+                {
+                    if (arr[i]%2==0 && arr[j]%2==0 && arr[i] < arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine();
+
+            foreach (int r in arr)
+            {
+                Console.Write(r+" ");
+            }
+
+        }
+    }
+}
